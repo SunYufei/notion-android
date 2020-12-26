e014ad289b33d2830ae966bebdea2fd82f314ebb
SunYufei <sunyufei963@gmail.com>
2020-12-26 15:49:26 +0800

    add WebView widget

--- app/src/main/java/ml/sunyufei/notion_android/MainActivity.kt
+++ app/src/main/java/ml/sunyufei/notion_android/MainActivity.kt
@@ -1,11 +1,36 @@
 package ml.sunyufei.notion_android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+    private val url = "https://www.notion.so/"
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+        // settings
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        // WebViewClient
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                view?.loadUrl(request?.url.toString())
+                return true
+            }
+        }
+        // load url
+        webView.loadUrl(url)
     }
 }
